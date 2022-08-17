5758a3c0d938213aaf1bf1add3dee4088a17120c
igar1991 <iiggaarr19912004@gmail.com>
2022-08-17 12:59:01 +0300

    init: project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
-import Button from "@mui/material/Button";
 import {
     BrowserRouter as Router,
     Routes,
