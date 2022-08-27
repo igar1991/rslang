b6f9dc37d5f9d24436c7fd35065d68b214d6a9c9
igar1991 <iiggaarr19912004@gmail.com>
2022-08-27 13:31:52 +0300

    fix: handler wordslevel

--- src/pages/vocabulary/vocabulary-words/words-view/words-levels/level-button.tsx
+++ src/pages/vocabulary/vocabulary-words/words-view/words-levels/level-button.tsx
@@ -1,21 +1,15 @@
 import { Button } from '@mui/material';
 import './levels.css';
-import { useDispatch } from 'react-redux';
-import { setGroup } from '../../../../../redux/slices/wordsSlice';
-import { useCallback } from 'react';
 import { Colors, ColorsByGroupMap } from './constants';
 import { LEVELS } from '../../constants';
 
 interface Props {
   level: LEVELS;
   group: number;
+  onClickHandler: (group: number)=>void
 }
 
-export const LevelButton = ({ level, group }: Props) => {
-  const dispatch = useDispatch();
-  const onClickHandler = useCallback(() => {
-    return dispatch(setGroup(group));
-  }, []);
+export const LevelButton = ({ level, group, onClickHandler }: Props) => {
   const color = ColorsByGroupMap.get(level) as Colors;
 
   return (
@@ -23,7 +17,7 @@ export const LevelButton = ({ level, group }: Props) => {
       variant='contained'
       color={color}
       className='level-button'
-      onClick={onClickHandler}
+      onClick={()=>onClickHandler(group)}
     >
       {level}
     </Button>
