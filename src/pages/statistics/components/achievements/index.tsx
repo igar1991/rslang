3f099d36022b40fbb6e1041e2c541fad236c444c
nensty <anastasiyasazonava@gmail.com>
2022-09-05 01:36:54 +0200

    feat: add achievements

--- src/pages/statistics/components/achievements/index.tsx
+++ src/pages/statistics/components/achievements/index.tsx
@@ -1,8 +1,24 @@
 import { Box, Typography } from '@mui/material';
 import './achievements.css';
 import { AchievementCard } from 'pages/statistics/components/achievements/achievement-card';
+import { wordsAPI } from 'api/wordsService';
+import { useAppSelector } from 'redux/hooks';
+import { selectAuth } from 'redux/slices/authUserSlice';
+import { useEffect, useState } from 'react';
+import { Achievement } from 'types/types';
 
 export const Achievements = () => {
+  const { id } = useAppSelector(selectAuth);
+  const { data } = wordsAPI.useGetUserStatisticsQuery(id);
+  const [achievements, setAchievements] = useState<Achievement[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      const achievementsArr = Object.entries(data.optional.achievements).map((entry) => entry[1]);
+      setAchievements(achievementsArr);
+    }
+  }, [data]);
+
   return (
     <Box className='achievements'>
       <Typography
@@ -13,7 +29,15 @@ export const Achievements = () => {
         My achievements
       </Typography>
       <Box className='achievements-wrapper'>
-        <AchievementCard />
+        {achievements.map(({ id, name, description, img, achieved }, index) => (
+          <AchievementCard
+            name={name}
+            description={description}
+            image={img}
+            key={id || index}
+            achieved={achieved}
+          />)
+        )}
       </Box>
     </Box>
   );
